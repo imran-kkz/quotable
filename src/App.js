b6a6bc48e1cf0a0b7efc2e2d436f68617e1fe80a
imran-kkz <imranrali1@gmail.com>
2021-02-01 22:44:09 -0500

    edit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,15 @@ import React from 'react'
 import data from './data'
 import Quote from './Quote'
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      value: ''
-    }
-  }
-
-  render() {
-    return(
-      <div className="App">
-        <h1>Quotable</h1>
-        <Quote/>
-      </div>
-    )
-  }
+function App() {
+  const allQuotes = data.map(quote => {
+    <Quote quote={data.quote} author={data.author}/>
+  })
+  return(
+    <div className="App">
+      <h1>Quotable</h1>
+    </div>
+  )
 }
 
 export default App;
