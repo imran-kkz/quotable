64ec091565f2d124faf46723a653f68cd21ad201
imran-kkz <imranrali1@gmail.com>
2021-02-03 23:59:10 -0500

    adding apicall into the mix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import './App.css';
-import React from 'react'
+import React, { Component } from 'react'
 import data from './data'
 import Quote from './Quote'
 
-function App() {
-  const allQuotes = data.map(item => <Quote key={item.id} quote={item.quote} author={item.author}/>)
+var a
 
-  return(
-    <div className="App">
-      {allQuotes}
-    </div>
-  )
+class App extends Component {
+  constructor() {
+    super()
+    this.apiCall = this.apiCall.bind(this);
+    this.state = {
+      quotes: data,
+      title: ""
+    }
+    // const allQuotes = data.map(item => <Quote key={item.id} quote={item.quote} author={item.author}/>)
+  }
+
+  // handleChange(response) {
+  //   this.setState({
+  //     title: response
+  //   })
+  // }
+
+  apiCall() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => {
+      this.setState({title:json.title})
+    })
+  }
+  componentDidMount() {
+    this.apiCall()
+  }
+
+  render(){
+    return(
+      <div className="App">
+        {this.state.quotes.map(item => <Quote quote={item.quote} author={item.author} key={item.id} />
+        )}
+        <p>{this.state.title}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
